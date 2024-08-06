a00eef0f6122ed52823db22f9560490bf7de02ac
JotaValla <jimmy.valladares@epn.edu.ec>
2024-08-05 20:52:35 -0500

    Feat: Se agrego documentacion de la api con swagger

--- src/main/java/com/jotacode/apigym/ApiGymApplication.java
+++ src/main/java/com/jotacode/apigym/ApiGymApplication.java
@@ -1,13 +1,32 @@
 package com.jotacode.apigym;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+
+@EnableWebMvc
 public class ApiGymApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGymApplication.class, args);
     }
+
+    @Bean
+    public OpenAPI customOpenAPI () {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Gym")
+                        .version("1.0")
+                        .description("API para un gimnasio")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
 
 }
